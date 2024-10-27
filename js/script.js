e65a3d86bcfcb28a67b7143d444b065ec95048a2
letizia <maukarletizia@gmail.com>
2024-10-27 01:51:37 -0700

    added user friednly chat box

--- js/script.js
+++ js/script.js
@@ -29,13 +29,65 @@ document.addEventListener('DOMContentLoaded', function(){
         const box = document.getElementById('shark-chat');
         box.style.visibility = 'visible';
 
-        const send = document.getElementById('send');
-        send.style.visibility = 'visible';
-
         shark.classList.add('horizonTranslate');
         shark.margin = "20%";
 
         console.log(shark)
         console.log(event.target)
     })
+
+  
+
+
+    set_outgoing(['LA','SB', 'OC'])
+
+    function set_outgoing(values) {
+      const outgoing = document.querySelector('.text.outgoing');
+      //alert(outgoing)
+      outgoing.innerHTML = "";
+
+      
+
+      // quest(['Hiya! Where county in California are we going to?'])
+      const incoming = document.querySelector('.text.incoming');
+      incoming.innerHTML = "";
+      incoming.appendChild(create_p("Hiya!<br> Which county in California are we going to?"));
+
+      // add button options
+      for (i in values) {
+        outgoing.appendChild(create_button(values[i]))
+      }
+    }
+  
+    function create_button(name) {
+      const newButton = document.createElement('button');
+
+      //newButton.attributes
+      newButton.innerHTML = name;
+
+      newButton.classList.add("answers");
+
+      newButton.addEventListener('click', function(e) {
+        set_outgoing(['Asian','Italian','American', 'Latina American'])
+
+        const incoming = document.querySelector('.text.incoming');
+        incoming.innerHTML = "";
+        incoming.appendChild(create_p("What kind of food are we craving?"));
+      })
+      return newButton;
+      // let outgoing = document.querySelector('li .text outgoing')
+      // outgoing.appendChild(newButton)
+      //document.body.appendChild(newButton);
+    }
+
+    function create_p(question) {
+      const newP = document.createElement('p');
+
+      newP.innerHTML = question;
+
+      newP.classList.add("shark-q");
+    
+      return newP
+    }
 })
+
